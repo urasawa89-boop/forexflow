04af1d3dde1e1a9d4eb5a75d84253bcf6d1718df
urasawa89-boop <urasawa89@gmail.com>
2026-03-25 15:02:15 +0900

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ export default function Document() {
     <Html>
       <Head>
         <link href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css" rel="stylesheet" />
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3106394601395095"
+     crossorigin="anonymous"></script>
       </Head>
       <body>
         <Main />
